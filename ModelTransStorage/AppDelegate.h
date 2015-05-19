ff77f38ae2a7076d742d089b1e942da073e7bab3
郑林琴 <zheng.linqin@hongfu001.com>
2015-05-19 21:02:02 +0800

    Initial Commit

---
+++ ModelTransStorage/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ModelTransStorage
+//
+//  Created by 郑林琴 on 15/5/19.
+//  Copyright (c) 2015年 郑林琴. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ModelTransStorageTests/ModelTransStorageTests.m
@@ -0,0 +1,40 @@
+//
+//  ModelTransStorageTests.m
+//  ModelTransStorageTests
+//
+//  Created by 郑林琴 on 15/5/19.
+//  Copyright (c) 2015年 郑林琴. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface ModelTransStorageTests : XCTestCase
+
+@end
+
+@implementation ModelTransStorageTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
